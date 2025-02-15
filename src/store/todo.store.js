6443d9c40fe3cc8747bfd17b079d47b2c5cf5199
Anderson Ortiz <ortizone620@gmail.com>
2025-02-15 12:26:44 -0600

    Metodos que ocuparemos (not implemented)

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,6 +20,44 @@ const initStore = () => {
     console.log('initStore 🥑');
 }
 
+const loadStore = () => {
+    throw new Error('not implemented');
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) => {
+    throw new Error('not implemented');
+}
+
+const toggleTodo = (todoId) => {
+    throw new Error('not implemented');
+}
+
+const deleteTodo = (todoId) => {
+    throw new Error('not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('not implemented');
+}
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('not implemented');
+}
+
 export default {
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
