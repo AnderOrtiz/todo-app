3ec4aec846a5ee9750c10ca5049788c111b9f2c9
Anderson Ortiz <ortizone620@gmail.com>
2025-02-21 10:01:28 -0600

    Validaciones y optimizaciones

--- src/use-cases/create-todo-html.js
+++ src/use-cases/create-todo-html.js
@@ -2,12 +2,21 @@ import { Todo } from "../todos/models/todo.model";
 
 
 export const createTodoHTML = (todo) => {
-    if(!todo) throw new Error('A TODO OBJECT IS REQUIRED');
+    if (!todo) throw new Error('A TODO OBJECT IS REQUIRED');
 
-    const html = `<h1>${ todo.description }</h1>`;
+    const html = `
+            <div class="view">
+                <input class="toggle" type="checkbox" ${todo.done ? 'checked' : ''}>
+                <label>${ todo.description }</label>
+                <button class="destroy"></button>
+            </div>
+            <input class="edit" value="Create a TodoMVC template">`;
 
     const liElement = document.createElement('li');
     liElement.innerHTML = html;
+    liElement.setAttribute('data-id', todo.id);
+
+    (todo.done) ? liElement.classList.add('completed'): '';
 
     return liElement;
 }
--- src/use-cases/render-todos.js
+++ src/use-cases/render-todos.js
@@ -1,11 +1,19 @@
 import { Todo } from "../todos/models/todo.model";
 import { createTodoHTML } from "./create-todo-html";
 
+let element;
+
 export const renderTodos = (elementId, todos = [] ) => {
-    
-    //TODO: referencia 
-    const element = document.querySelector(elementId);
+
+
+    if(!element)
+        element = document.querySelector(elementId);
+
+    if(!element) throw new Error(`Element ${ elementId} not fount`)
+
+        element.innerHTMl = '';
+
     todos.forEach( todo => {
-        element.append(createTodoHTML(todo))
+        element.append(createTodoHTML(todo));
     });
 }
