0e5bdaa37e5cbba2c45861fb6790d30e59029e89
Anderson Ortiz <ortizone620@gmail.com>
2025-02-21 11:05:32 -0600

    crear un nuevo todo

--- src/todos/app.js
+++ src/todos/app.js
@@ -2,8 +2,9 @@ import { renderTodos } from '../use-cases';
 import html from './app.html?raw'
 import todoStore from '../store/todo.store'
 
-const elementIDs = {
+const ElementIDs = {
     TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
 }
 
 
@@ -15,7 +16,7 @@ export const App = (elementId) => {
 
     const displayTodos = () => {
         const todos = todoStore.getTodos(todoStore.getCurrentFilter());
-        renderTodos(elementIDs.TodoList, todos);
+        renderTodos(ElementIDs.TodoList, todos);
 }
 
     (() => {
@@ -24,6 +25,18 @@ export const App = (elementId) => {
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
         displayTodos();
+
     })();
+
+    const newDescriptionInput = document.querySelector(ElementIDs.NewTodoInput);
+
+    newDescriptionInput.addEventListener('keyup', (event) => {
+        if (event.keyCode !== 13) return;
+        if (event.target.value.trim().length === 0 ) return;
+
+        todoStore.addTodo(event.target.value);
+        displayTodos();
+        event.target.value = '';
+    });
 
 }
--- src/use-cases/render-todos.js
+++ src/use-cases/render-todos.js
@@ -3,15 +3,19 @@ import { createTodoHTML } from "./create-todo-html";
 
 let element;
 
+/**
+ * 
+ * @param {uuid} elementId 
+ * @param {Todo} todos 
+ */
 export const renderTodos = (elementId, todos = [] ) => {
-
 
     if(!element)
         element = document.querySelector(elementId);
 
-    if(!element) throw new Error(`Element ${ elementId} not fount`)
+    if(!element) throw new Error(`Element ${ elementId} not fount`);
 
-        element.innerHTMl = '';
+    element.innerHTML = '';
 
     todos.forEach( todo => {
         element.append(createTodoHTML(todo));
